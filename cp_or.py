f3ea4e657c10647e2b8e8526994c6b1d4772442e
Matteo Fasulo <mat.fasulo@gmail.com>
2024-05-15 20:32:58 +0200

    chore: Update matplotlib dependency and fix check_solution error
    Co-authored-by: Antonio Gravina <GravAnt@users.noreply.github.com>

--- cp_or.py
+++ cp_or.py
@@ -1,9 +1,31 @@
+import os, re, json
 from ortools.sat.python import cp_model
-import matplotlib.pyplot as plt
 import numpy as np
 
 from utils import read_instance, parser_obj
 
+# https://how-to.aimms.com/Articles/332/332-Formulation-CVRP.html
+
+def path_sequence_cost(path, D, as_solution:bool=False):
+    """
+    Calculate the cost of a path sequence
+    """
+    cost = 0
+    for i,j in path:
+        cost += D[i][j]
+    return cost
+
+def preprocess(D):
+    """
+    Preprocess the distance matrix
+    """
+    # load it as a numpy array
+    D = np.array(D)
+    D = np.insert(D, 0, D[-1], axis=0)
+    D = np.delete(D, -1, axis=0)
+    D = np.insert(D, 0, D[:, -1], axis=1)
+    D = np.delete(D, -1, axis=1)
+    return D
 
 def main():
     args = parser_obj()
@@ -16,68 +38,100 @@ def main():
     D = instance['D']
     NODES = ITEMS + 1
 
+    # Preprocess the distance matrix
+    D = preprocess(D)
+
+    print(D)
+
     model = cp_model.CpModel()
 
-    # Decision Variables
-    DISTANCE_MATRIX = []
-    for k in range(COURIERS):
-        DISTANCE_MATRIX.append([])
-        for i in range(NODES):
-            DISTANCE_MATRIX[k].append([])
-            for j in range(NODES):
-                DISTANCE_MATRIX[k][i].append(model.NewBoolVar(name=f"c_{k}_{i}_{j}"))
-    
-    # Convert it to a numpy array
-    DISTANCE_MATRIX = np.array(DISTANCE_MATRIX)
-    D = np.array(D)
+    # Decision variables
+    TENSOR = []
+    for i in range(NODES):
+        TENSOR.append([])
+        for j in range(NODES):
+            TENSOR[i].append([])
+            for k in range(COURIERS):
+                TENSOR[i][j].append(model.NewBoolVar(f'x_{i}_{j}_{k}'))
 
     # Constraints
-    # Each item is delivered by exactly one courier - aka each row/column has exactly one 1 in the 3D matrix
-    for i in range(ITEMS):
-        model.AddExactlyOne([DISTANCE_MATRIX[k][i][j] for k in range(COURIERS) for j in range(NODES) if j != i])
-        model.AddExactlyOne([DISTANCE_MATRIX[k][j][i] for k in range(COURIERS) for j in range(NODES) if j != i])
+    # 1) Vehicle leaves node it enters
+    for j in range(NODES):
+        for k in range(COURIERS):
+            model.Add(sum(TENSOR[i][j][k] for i in range(NODES)) == sum(TENSOR[j][i][k] for i in range(NODES)))
 
-    # All the couriers leave the depot
+    # 2) Each node is entered just once by any vehicle
+    for j in range(1, NODES):
+        model.Add(sum(TENSOR[i][j][k] for i in range(NODES) for k in range(COURIERS)) == 1)
+
+    # 3) Every vehicle starts from the depot and ends at the depot
     for k in range(COURIERS):
-        model.Add(DISTANCE_MATRIX[k][ITEMS][ITEMS] == 0)
-        #model.Add(DISTANCE_MATRIX[k][NODES-1][0] == 1)
+        model.Add(sum(TENSOR[0][j][k] for j in range(1, NODES)) == 1)
+        #model.Add(sum(TENSOR[j][0][k] for j in range(1, NODES)) == 1)
 
-    # Weight constraint for each courier
-    #for k in range(COURIERS):
-    #    model.Add(sum([SIZE[i] * DISTANCE_MATRIX[k][i][j] for i in range(NODES) for j in range(NODES)]) <= MAX_LOAD[k])
+    # 4) Capacity constraints
+    for k in range(COURIERS):
+        model.Add(sum(SIZE[j] * TENSOR[i][j][k] for j in range(1, NODES) for i in range(NODES)) <= MAX_LOAD[k])
+
+    # 5) Remove self-loops
+    for i in range(NODES):
+        for k in range(COURIERS):
+            model.Add(TENSOR[i][i][k] == 0)
+
+    # 6) Do not go come back to previous node
+    for k in range(COURIERS):
+        for i in range(1, NODES):
+            for j in range(1, NODES):
+                model.Add(TENSOR[i][j][k] + TENSOR[j][i][k] <= 1)
+
+    # Objective function: minimize the maximum distance traveled by any vehicle
+    arr_dist = []
+    for k in range(COURIERS):
+        arr_dist.append(sum(D[i][j] * TENSOR[i][j][k] for i in range(NODES) for j in range(NODES)))
     
-    # Circuit constraint for each courier
-    # Create the circuit constraint.
-    arcs = []
-    for k in range(COURIERS):
-        for i in range(NODES):
-            for j in range(NODES):
-                if i != j:
-                    arcs.append([i, j, DISTANCE_MATRIX[k][i][j]])
+    obj = model.NewIntVar(0, sum(sum(row) for row in D), 'max_distance')
+    model.AddMaxEquality(obj, arr_dist)
 
-    model.AddCircuit(arcs)
-
-    # OBJECTIVE FUNCTION - minimize the maximum distance travelled by any courier
-    #obj = cp_model.LinearExpr.Sum([DISTANCE_MATRIX[k][i][j]*D[i, j] for k in range(COURIERS) for i in range(NODES) for j in range(NODES)])
-
-    # MINIMIZATION OF THE OBJ FUNCTION
-    #model.Minimize(obj)
+    # Minimize the objective function
+    model.Minimize(obj)
+    
     solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 300.0 # Time Limit of 5 minutes
-    #solver.parameters.log_search_progress = True
+    solver.parameters.max_time_in_seconds = 300
+    solver.parameters.log_search_progress = True
+    #solver.parameters.num_search_workers = 1
     #solver.parameters.linearization_level = 2
     status = solver.Solve(model)
 
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print(f'Objective value: {solver.ObjectiveValue()}')
 
-        # Print the stats
         print(solver.ResponseStats())
 
-        # Print the true variables for each courier
-        print(np.array([[[solver.Value(DISTANCE_MATRIX[k][i][j]) for i in range(NODES)] for j in range(NODES)] for k in range(COURIERS)]))
+        for k in range(COURIERS):
+            path = []
+            for i in range(NODES):
+                for j in range(NODES):
+                    if solver.Value(TENSOR[i][j][k]) == 1:
+                        path.append((i,j))
+                        print(f'Courier {k} travels from node {i} to node {j}')
+            print(f'Path sequence: {path}, cost: {path_sequence_cost(path, D)}')
+            print('\n')
+
+        # Extract digit from string
+        num = int(re.search('\d+', args.instance).group())
+        with open(f'{os.getcwd()}{os.sep}res{os.sep}SAT{os.sep}{num}.json', 'w') as file:
+            json.dump({
+                'gecode':{ #TODO: change this, just to check the solution
+                    'time': solver.WallTime(),
+                    'optimal': status == cp_model.OPTIMAL,
+                    'obj': int(solver.ObjectiveValue()),
+                    # list of lists of paths
+                    'sol': [[j for i in range(NODES-1) for j in range(1, NODES) if solver.Value(TENSOR[i][j][k]) == 1] for k in range(COURIERS)]
+                }
+            }, file, indent=4)
 
     else:
-        print("No solution found")
+        print('No solution found')
 
 if __name__ == '__main__':
     main()
